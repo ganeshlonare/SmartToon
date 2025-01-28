84c15353b9706bb49c068ffd215c64f77454e737
Apeksha Bhausaheb LOkhande <apekshalokhande8600@gmail.com>
2025-01-28 19:06:23 +0530

    server setup is done using express js and mongodb data base

---
+++ backened/server.js
@@ -0,0 +1,9 @@
+
+const PORT=process.env.PORT||8000;
+import app from "./app.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`);
+})
